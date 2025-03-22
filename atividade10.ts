942666e0f7575d5ec85963be00b52be3946453c1
laaiszn <laiszanqueta4@gmail.com>
2025-03-22 16:35:01 -0300

    Update atividade10.ts

--- atividade10.ts
+++ atividade10.ts
@@ -1,15 +1,39 @@
 let teclado = require('prompt-sync')();
-let n1 = parseFloat(teclado('Digite a nota da N1: '));
-let n2 = parseFloat(teclado('Digite a nota da N2: '));
-let media = (n1 + n2) / 2;
-if (media >= 8.5 && media <= 10) {
-    console.log('A');
-} else if (media >= 7 && media < 8.5) {
-    console.log('B');
-} else if (media >= 5 && media < 7) {
-    console.log('C');
-} else if (media >= 3 && media < 5) {
-    console.log('D');
-} else {
-    console.log('E');
+let respostasPositivas: number = 0;
+ let resposta1: string | null = prompt("Você telefonou para a vítima? (sim/não)");
+    if (resposta1 === "sim") {
+        respostasPositivas++;
+    }
+
+    let resposta2: string | null = prompt("Você esteve no local do crime? (sim/não)");
+    if (resposta2 === "sim") {
+        respostasPositivas++;
+    }
+
+    let resposta3: string | null = prompt("Você mora perto da vítima? (sim/não)");
+    if (resposta3 === "sim") {
+        respostasPositivas++;
+    }
+
+    let resposta4: string | null = prompt("Você devia para a vítima? (sim/não)");
+    if (resposta4 === "sim") {
+        respostasPositivas++;
+    }
+
+    let resposta5: string | null = prompt("Você já trabalhou com a vítima? (sim/não)");
+    if (resposta5 === "sim") {
+        respostasPositivas++;
+    }
+
+    let classificacao: string = "Inocente";
+    if (respostasPositivas === 2) {
+        classificacao = "Suspeita";
+    } else if (respostasPositivas >= 3 && respostasPositivas <= 4) {
+        classificacao = "Cúmplice";
+    } else if (respostasPositivas === 5) {
+        classificacao = "Assassino";
+    }
+
+    console.log("Você foi classificado como:", classificacao);
+    alert("Você foi classificado como: " + classificacao);
 }
